832dc4e090c9abbfe358154da77012fcc60c7538
Francisco Salomon <fsalomon@mathworks.com>
2024-05-24 16:19:41 +0100

    Added a catch for checkpoint generation in order to continue if failed since isn't critical

--- fpga/usrp3/tools/scripts/viv_utils.tcl
+++ fpga/usrp3/tools/scripts/viv_utils.tcl
@@ -87,7 +87,9 @@ proc ::vivado_utils::initialize_project {} {
         } elseif [expr [lsearch {.xci} $src_ext] >= 0] {
             puts "BUILDER: Adding IP      : $src_file"
             read_ip $src_file
-            set_property generate_synth_checkpoint true [get_files $src_file]
+            if {[catch {set_property generate_synth_checkpoint true [get_files $src_file]} errorstring]} {
+                puts "BUILDER: Failed to set synth checkpoint generation ($errorstring). The process will continue."
+            }
         } elseif [expr [lsearch {.ngc .edif .edf} $src_ext] >= 0] {
             puts "BUILDER: Adding Netlist : $src_file"
             read_edif $src_file
